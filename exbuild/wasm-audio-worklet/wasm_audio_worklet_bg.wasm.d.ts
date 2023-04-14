578ff784c2224cb20020dec2903f8c4c6753ea3f
namse <3580430+namse@users.noreply.github.com>
2023-04-14 16:52:26 +0000

    Deploying to gh-pages from @ namse/wasm-bindgen@4ce6b8c084e0afbdf313bcaa526267846e34268a 🚀

---
+++ exbuild/wasm-audio-worklet/wasm_audio_worklet_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export function __wbg_wasmaudioprocessor_free(a: number): void;
+export function wasmaudioprocessor_process(a: number, b: number, c: number, d: number): number;
+export function wasmaudioprocessor_pack(a: number): number;
+export function wasmaudioprocessor_unpack(a: number): number;
+export function web_main(): number;
+export const memory: WebAssembly.Memory;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h43a127b3dac86cb5(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3740b0f32bd8d14e(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h176c400fbf4ab0ae(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_thread_destroy(a: number, b: number): void;
+export function __wbindgen_start(): void;
